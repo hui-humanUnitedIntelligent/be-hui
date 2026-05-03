a1ac47b6ec57676fc7032cb0afe44345f3b61010
Lars Gutknecht <4visionglobal.faireint@gmail.com>
2026-05-03 10:31:19 +0000

    Update app pages

--- src/pages/BookingFlow.jsx
+++ src/pages/BookingFlow.jsx
@@ -1,3 +1,5 @@
-export default function BookingFlow() {
+// This component is defined internally in Home.jsx
+// This file is a placeholder only
+export default function BookingFlowPage() {
   return null;
 }
