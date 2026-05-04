b2bf7f95e476b5acc1641dd90e6e6e02f1c06ec7
hui-humanUnitedIntelligent <huiwirken@gmail.com>
2026-05-04 23:23:46 +0200

    feat: add /auth/callback route for email confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import LoginPage from './pages/LoginPage'
 import BookingFlow from './pages/BookingFlow'
 import Admin from './pages/Admin'
+import AuthCallback from './pages/AuthCallback'
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated, isLoading } = useAuth()
@@ -20,6 +21,7 @@ function AppRoutes() {
   const { isAuthenticated } = useAuth()
   return (
     <Routes>
+      <Route path="/auth/callback" element={<AuthCallback />} />
       <Route path="/login" element={isAuthenticated ? <Navigate to="/Home" replace /> : <LoginPage onSuccess={() => window.location.href = '/Home'} />} />
       <Route path="/" element={<Navigate to="/Home" replace />} />
       <Route path="/Home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
