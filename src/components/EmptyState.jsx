c062788924db86593f4572e5724033b55c7e0cf1
hui-humanUnitedIntelligent <huiwirken@gmail.com>
2026-05-05 13:48:53 +0200

    feat: extract EmptyState component

---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+  return (
+    <div style={{ textAlign: "center", padding: "48px 24px" }}>
+      <div style={{ fontSize: 44, marginBottom: 12 }}>{icon}</div>
+      <div style={{ fontWeight: 700, fontSize: 16, color: "#333", marginBottom: 6 }}>{label}</div>
+      {sub && <div style={{ fontSize: 13, color: "#aaa", lineHeight: 1.5 }}>{sub}</div>}
+    </div>
+  );
+}
+
+function SectionHeader({ title, onBack }) {
+
+export default EmptyState;
