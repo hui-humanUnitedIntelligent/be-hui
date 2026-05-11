96ae2283f75cac9fb15886ac885892d0da37898c
hui-humanUnitedIntelligent <huiwirken@gmail.com>
2026-05-11 12:17:18 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './App'
+import './index.css'
+
+window.onerror = function(message, source, lineno, colno, error) {
+  document.body.innerHTML = `
+    <div style="padding:20px;font-family:Arial;background:#fff;color:#000;">
+      <h1>React Error</h1>
+      <pre>${message}</pre>
+      <pre>${error?.stack || ''}</pre>
+    </div>
+  `;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
