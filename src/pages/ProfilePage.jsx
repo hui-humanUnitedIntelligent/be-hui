027d9590d3d00f6514eb5dbe045af8a5c26a4f59
hui-humanUnitedIntelligent <huiwirken@gmail.com>
2026-05-16 22:32:19 +0200

    fix(build): correct import path in src/pages/ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../lib/AuthContext";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from ".../lib/supabaseClient";
 import {
   BestellungenPage,
   NachrichtenPage,
