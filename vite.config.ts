90856f12143b0dd06ae92e8f2581df4dec1c8530
hui-humanUnitedIntelligent <huiwirken@gmail.com>
2026-05-16 20:52:04 +0200

    chore: final rebuild 2026-05-16 18:52:03

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-// HUI build 2026-05-16 18:50:37 — phase2d complete
+// HUI build 2026-05-16 18:52:03 — phase2d full complete
 import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
