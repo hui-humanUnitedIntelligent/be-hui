676631388d1b5c17d8254aee21c859a0268e5c06
HUI-HumanUnitedIntelligent <281621904+hui-humanUnitedIntelligent@users.noreply.github.com>
2026-06-09 21:03:25 +0300

    feat(sprint-c): sections barrel export

---
+++ src/components/profile/sections/index.js
@@ -0,0 +1,11 @@
+// src/components/profile/sections/index.js
+// Barrel-Export aller unified Profile Sections (Sprint C)
+export { AboutSection }           from "./AboutSection.jsx";
+export { TalentSection }          from "./TalentSection.jsx";
+export { WorksSection }           from "./WorksSection.jsx";
+export { ExperiencesSection }     from "./ExperiencesSection.jsx";
+export { RecommendationsSection } from "./RecommendationsSection.jsx";
+export { AvailabilitySection }    from "./AvailabilitySection.jsx";
+export { LocationSection }        from "./LocationSection.jsx";
+export { VisibilitySection }      from "./VisibilitySection.jsx";
+export { MomentsSection }         from "./MomentsSection.jsx";
