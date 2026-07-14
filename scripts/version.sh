bdd83d94b35dae580b7da2a18824982a38065028
Michael <DEINE_GITHUB_EMAIL>
2026-07-14 19:02:53 +0300

    Release Build

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-VERSION=$(jq -r '.version' package.json)
-NEW_VERSION=$(echo $VERSION | awk -F. -v OFS=. '{$NF++; print}')
-jq ".version=\"$NEW_VERSION\"" package.json > package.tmp.json
-mv package.tmp.json package.json
+
+VERSION=$(grep '"version"' package.json | sed -E 's/.*"version": "([0-9]+\.[0-9]+\.[0-9]+)".*/\1/')
+IFS='.' read -r major minor patch <<< "$VERSION"
+NEW_PATCH=$((patch + 1))
+NEW_VERSION="$major.$minor.$NEW_PATCH"
+
+sed -i "s/\"version\": \"$VERSION\"/\"version\": \"$NEW_VERSION\"/" package.json
+
 echo "Neue Version: $NEW_VERSION"
