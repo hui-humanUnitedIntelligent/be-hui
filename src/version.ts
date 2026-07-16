f158c0fccbe633cdae5619cf843d501b8e8f793e
Michael <DEINE_GITHUB_EMAIL>
2026-07-16 17:52:31 +0300

    Release Build

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.0.2";
-export const APP_VERSION_CODE = 2;
+export const APP_VERSION = "2.0.2 Beta";
+export const APP_VERSION_CODE = 202;
 
 export default APP_VERSION;
