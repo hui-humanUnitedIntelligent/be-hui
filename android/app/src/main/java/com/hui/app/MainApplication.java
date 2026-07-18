13b061ee2b93595928aba3c04e827e3cc1bb24dc
Michael <DEINE_GITHUB_EMAIL>
2026-07-18 15:03:42 +0300

    Auto-Release: Version 2.0.13

--- android/app/src/main/java/com/hui/app/MainApplication.java
+++ android/app/src/main/java/com/hui/app/MainApplication.java
@@ -1,26 +1,11 @@
 package com.hui.app;
 
 import android.app.Application;
-import com.getcapacitor.Capacitor;
 
 public class MainApplication extends Application {
-
     @Override
     public void onCreate() {
         super.onCreate();
-
-        // ---- Capacitor Initialisierung ----
-        Capacitor.init(this);
-
-        // ---- WebView Stabilität ----
-        Capacitor.getBridge().getWebView().getSettings().setDomStorageEnabled(true);
-        Capacitor.getBridge().getWebView().getSettings().setDatabaseEnabled(true);
-
-        // ---- Plugin Stabilität ----
-        Capacitor.getPlugins();
-
-        // ---- Performance ----
-        Capacitor.getBridge().getWebView().getSettings().setAllowFileAccess(true);
-        Capacitor.getBridge().getWebView().getSettings().setAllowContentAccess(true);
+        // Keine Capacitor-Initialisierung notwendig
     }
 }
--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.0.12 Beta";
-export const APP_VERSION_CODE = 212;
+export const APP_VERSION = "2.0.13 Beta";
+export const APP_VERSION_CODE = 213;
 
 export default APP_VERSION;
