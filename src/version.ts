3c76d16520805d1cb066c36b1798b4f4ceb7f66d
Michael <DEINE_GITHUB_EMAIL>
2026-07-18 09:19:05 +0300

    Auto-Release: Version 2.0.10

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.0.9 Beta";
-export const APP_VERSION_CODE = 209;
+export const APP_VERSION = "2.0.10 Beta";
+export const APP_VERSION_CODE = 210;
 
 export default APP_VERSION;
