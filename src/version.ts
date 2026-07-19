c0753c77720546967a851b8c73ea5a7c2d0e789d
Michael <DEINE_GITHUB_EMAIL>
2026-07-20 00:20:25 +0300

    Auto-Release: Version 2.0.15

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.0.14 Beta";
-export const APP_VERSION_CODE = 214;
+export const APP_VERSION = "2.0.15 Beta";
+export const APP_VERSION_CODE = 215;
 
 export default APP_VERSION;
