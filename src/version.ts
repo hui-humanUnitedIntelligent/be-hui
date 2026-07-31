3c6fc884860aca7e81b84616e6a2c4af5c4fe9d0
Michael <DEINE_GITHUB_EMAIL>
2026-07-31 13:42:59 +0300

    Version bump + Web-Build

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.0.19 Beta";
-export const APP_VERSION_CODE = 219;
+export const APP_VERSION = "1.0.1";
+export const APP_VERSION_CODE = 2;
 
 export default APP_VERSION;
