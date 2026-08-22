c61f3f2b50f771148f433f1c8a74b4b2121a620e
Michael S <ms88@hotmail.de>
2026-08-22 15:33:37 +0000

    diag: BrowserRouter + AuthProvider + ToastContainer + div

--- src/web-main.jsx
+++ src/web-main.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './lib/AuthContext.jsx';
+import { ToastContainer } from './lib/useToast.jsx';
 import './index.css';
 import './web.css';
+import './landing.css';
 
 const _diag = document.getElementById('diag');
-if (_diag) _diag.innerHTML += '<br>[JS] Test 1: BrowserRouter + div';
+if (_diag) _diag.innerHTML += '<br>[JS] Test 2: BrowserRouter + AuthProvider + div';
 
 try {
   ReactDOM.createRoot(document.getElementById('web-root')).render(
     React.createElement(BrowserRouter, { basename: '/app' },
-      React.createElement('div', { style: { padding: 40, fontSize: 24 } }, 'BrowserRouter works!')
+      React.createElement(AuthProvider, null,
+        React.createElement(React.Fragment, null,
+          React.createElement(ToastContainer),
+          React.createElement('div', { style: { padding: 40, fontSize: 24 } }, 'AuthProvider works!')
+        )
+      )
     )
   );
   _diag && (_diag.innerHTML += '<br>[JS] render() called');
@@ -18,7 +26,9 @@ try {
   _diag && (_diag.innerHTML += '<br>[JS] ❌ CRASH: ' + e.message);
 }
 
-setTimeout(function() {
-  var root = document.getElementById('web-root');
-  _diag && (_diag.innerHTML += '<br>[1s] children: ' + (root ? root.childNodes.length : 'NULL'));
-}, 1000);
+[500, 1000, 2000].forEach(function(delay) {
+  setTimeout(function() {
+    var root = document.getElementById('web-root');
+    _diag && (_diag.innerHTML += '<br>[' + delay + 'ms] children: ' + (root ? root.childNodes.length : 'NULL'));
+  }, delay);
+});
