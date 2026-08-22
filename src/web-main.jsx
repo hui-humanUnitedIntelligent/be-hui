ac154923d4731b38b8604fafbf25858429599ea2
Michael S <ms88@hotmail.de>
2026-08-22 17:14:20 +0000

    diag: render WebApp without StrictMode/ErrorBoundary

--- src/web-main.jsx
+++ src/web-main.jsx
@@ -5,44 +5,21 @@ import { GlobalAppBoundary } from './lib/ErrorBoundaries.jsx';
 import './index.css';
 import './web.css';
 import './landing.css';
-import { initSentry } from './lib/sentry.js';
-import { initGlobalKeyboardHandling } from "./lib/globalKeyboardHandler.js";
 
 const _d = document.getElementById('diag');
 if (_d) _d.innerHTML = '[JS] module loaded, React=' + React.version;
 
-try { initSentry(); } catch(e) {}
-try { initGlobalKeyboardHandling(); } catch(e) {}
-
 const rootEl = document.getElementById('web-root');
+
+// Test 1: Just WebApp, no StrictMode, no ErrorBoundary
 const r = ReactDOM.createRoot(rootEl);
-
-r.render(
-  <React.StrictMode>
-    <GlobalAppBoundary>
-      <WebApp />
-    </GlobalAppBoundary>
-  </React.StrictMode>
-);
-
+r.render(<WebApp />);
 if (_d) _d.innerHTML += '\n[JS] render() called';
 
-// Inspect DOM after 3s
 setTimeout(() => {
   if (_d) {
-    _d.innerHTML += '\n[3s] #web-root children=' + rootEl.childElementCount;
+    _d.innerHTML += '\n[3s] children=' + rootEl.childElementCount;
     _d.innerHTML += '\n[3s] innerHTML.len=' + rootEl.innerHTML.length;
-    _d.innerHTML += '\n[3s] innerHTML.preview=' + rootEl.innerHTML.substring(0, 300);
-    // Check for error boundary UI
-    const errUI = rootEl.querySelector('[style*="background"]');
-    _d.innerHTML += '\n[3s] has error UI=' + !!errUI;
-    // Check for loading screen
-    const loadingEl = rootEl.querySelector('.web-loading');
-    _d.innerHTML += '\n[3s] has .web-loading=' + !!loadingEl;
-    // Check all class names
-    const allEls = rootEl.querySelectorAll('*');
-    const classes = new Set();
-    allEls.forEach(el => { if(el.className) el.classList.forEach(c => classes.add(c)); });
-    _d.innerHTML += '\n[3s] classes=' + [...classes].slice(0, 15).join(',');
+    _d.innerHTML += '\n[3s] preview=' + rootEl.innerHTML.substring(0, 500);
   }
 }, 3000);
